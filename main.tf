2672ec08d2f05acdffdb9271b9b5eaaf74a3c3a5
Daniel Richter <Nexyz9@gmail.com>
2022-04-06 22:46:42 -0700

    Implement ignoring task definitions

--- main.tf
+++ main.tf
@@ -177,6 +177,10 @@ resource aws_ecs_service service {
   # AWS Accounts need to opt-in to allowing tagging of this resource, else this will
   # cause the resource creation to fail.
   tags = var.ecs_service_tagging_enabled ? local.tags : null
+  
+  lifecycle {
+    ignore_changes = var.ignore_task_definition ? ["task_definition"]  : []
+  }
 }
 
 /**
